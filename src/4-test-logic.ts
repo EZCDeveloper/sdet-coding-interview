fdb206652a324ad35367be455a306c76a51da20d
Emanuel Zini Casaro <emanuel.zini-casaro@appdirect.com>
2023-10-31 13:17:15 -0300

    created  test 4

--- src/4-test-logic.ts
+++ src/4-test-logic.ts
@@ -1,7 +1,12 @@
 const numList = [98, 1, 2, 10, 2, 9, 16];
 
 function sortArray(arr: any) {
-  
+  numList.sort(function (a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  });
+  console.log(numList);
 }
 
 sortArray(numList);
