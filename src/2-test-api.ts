ebe3e82180e61c28a21662eff94d2deb0e779e37
Emanuel Zini Casaro <emanuel.zini-casaro@appdirect.com>
2023-10-31 15:25:26 -0300

    refactored Ejercicio #2

--- src/2-test-api.ts
+++ src/2-test-api.ts
@@ -1,17 +1,19 @@
 import axios from 'axios';
 import expect from 'expect';
 
-const BASE_URL = 'https://fakestoreapi.com';
-
 require('dotenv').config();
 
 async function testApi() {
-  const res = await axios.post(BASE_URL + '/auth/login', {
-    username: 'mor_2314',
-    password: '83r5^_',
-  });
-  console.log(res.data.token);
+  const BASE_URL = process.env.BASE_URL;
+  const username = process.env.USERNAME;
+  const password = process.env.PASSWORD;
 
+  const res = await axios.post(`${BASE_URL}/auth/login`, {
+    username,
+    password,
+  });
+
+  console.log(res.data.token);
   expect(res.status).toEqual(200);
 }
 
